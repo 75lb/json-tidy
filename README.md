d0500d131f4857285227e7fe5093377b2cf6860b
Lloyd Brookes <lloyd.brookes@gmail.com>
2015-12-26 00:35:23 +0000

    replace transform-tools

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 [![view on npm](http://img.shields.io/npm/v/json-tidy.svg)](https://www.npmjs.org/package/json-tidy)
 [![npm module downloads per month](http://img.shields.io/npm/dm/json-tidy.svg)](https://www.npmjs.org/package/json-tidy)
 [![Dependency Status](https://david-dm.org/75lb/json-tidy.svg)](https://david-dm.org/75lb/json-tidy)
+[![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg)](https://github.com/feross/standard)
 
 json-tidy
 =========
--- bin/cli.js
+++ bin/cli.js
@@ -1,24 +1,22 @@
 #!/usr/bin/env node
-"use strict";
-var tr = require("transform-tools");
-var domain = require("domain");
-var ansi = require("ansi-escape-sequences");
+'use strict'
+var collectJson = require('collect-json')
+var domain = require('domain')
+var ansi = require('ansi-escape-sequences')
 
-function tidy(json){
-    return JSON.stringify(json, null, "  ") + "\n";
+function tidy (json) {
+  return JSON.stringify(json, null, '  ') + '\n'
 }
 
-function halt(err){
-    console.error(ansi.format(err.message, "red"));
-    console.error(ansi.format("Input", [ "underline", "red" ]));
-    console.error(ansi.format(err.input, "red"));
-    process.exit(1);
+function halt (err) {
+  console.error(ansi.format(err.message, 'red'))
+  process.exit(1)
 }
 
-var d = domain.create();
-d.on("error", halt);
-d.run(function(){
-    process.stdin
-        .pipe(tr.collectJson({ through: tidy }))
-        .pipe(process.stdout);
-});
+var d = domain.create()
+d.on('error', halt)
+d.run(function () {
+  process.stdin
+    .pipe(collectJson(tidy))
+    .pipe(process.stdout)
+})
